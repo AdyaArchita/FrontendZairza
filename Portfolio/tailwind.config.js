70d0224a7179a4cc0f414b332870684e21450635
AdyaArchita <adyaarchitapattnaik@gmail.com>
2025-08-05 01:58:18 +0530

    Added Dependencies and others

---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        k2d: ['"K2D"', 'sans-serif'],
+        lato: ['"Lato"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
